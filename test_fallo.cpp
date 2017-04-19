e652f7ed561a6628bbc92282a0a35be3ee82c0de
cwen0 <cwenyin0@gmail.com>
2017-04-19 14:00:06 +0800

    update

--- test_fallo.cpp
+++ test_fallo.cpp
@@ -1,59 +1,21 @@
-//#include "io_posix.h"
-#include <errno.h>
+#include <linux/falloc.h>
 #include <fcntl.h>
-#include <algorithm>
-#include <linux/fs.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
+#include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/statfs.h>
-#include <sys/syscall.h>
 #include <unistd.h>
-#include <sys/types.h>
-
-//#include "posix_logger.h"
-//#include "monitoring/iostats_context_imp.h"
-//#include "port/port.h"
-//#include "rocksdb/slice.h"
-//#include "util/coding.h"
-//#include "util/string_util.h"
-//#include "util/sync_point.h"
-
-#define IOSTATS_TIMER_GUARD(metric)
-bool allow_fallocate_;
+#include <stdio.h>
+#include <string>
 
 int main(int argc, char *argv[]) {
-  //int flags = O_CREAT | O_APPEND | O_WRONLY;
-  //int fd = open("testfile", flags, 0644);
-  //int alloc_status = fallocate(fd, FALLOC_FL_KEEP_SIZE, 0, 9192);
-  //std::string buf(5000, 'a');
-  //ssize_t done = write(fd, buf.data(), 5000);
-  //if (done != 5000) printf("Error\n");
-  //int r = ftruncate(fd, 5000);
-  //if (r == -1) printf("Error\n");
-  //r = fallocate(fd, FALLOC_FL_KEEP_SIZE | FALLOC_FL_PUNCH_HOLE, 5000, 4192);
-  //if (r == -1) printf("Error\n");
-  //close(fd);
-  //
-  //int fd_;
   int flags = O_CREAT | O_APPEND | O_WRONLY;
-  int fd_ = open("testfile", flags, 0644);
-  int alloc_status = fallocate(fd_, FALLOC_FL_KEEP_SIZE, 0, 9192);
+  int fd = open("testfile", flags, 0644);
+  int alloc_status = fallocate(fd, FALLOC_FL_KEEP_SIZE, 0, 9192);
   std::string buf(5000, 'a');
-  ssize_t done = write(fd_, buf.data(), 5000);
+  ssize_t done = write(fd, buf.data(), 5000);
   if (done != 5000) printf("Error\n");
-  int r = ftruncate(fd_, 5000);
+  int r = ftruncate(fd, 5000);
   if (r == -1) printf("Error\n");
-  struct stat file_stats;
-
-  allow_fallocate_ = true;
-  fstat(fd_, &file_stats);
-  // After ftruncate, we check whether ftruncate has the correct behavior.
-  // If not, we should hack it with FALLOC_FL_PUNCH_HOLE
-  static bool buggy = false;
-  fallocate(fd_, FALLOC_FL_KEEP_SIZE | FALLOC_FL_PUNCH_HOLE, 5000, 4192);
+  r = fallocate(fd, FALLOC_FL_KEEP_SIZE | FALLOC_FL_PUNCH_HOLE, 5000, 4192);
+  if (r == -1) printf("Error\n");
+  close(fd);
 }
